97f1f5802ad171d3f2f3cdb3a468e24c7aaee969
Angel Castro <angelajco@gmail.com>
2021-08-29 17:50:07 -0500

    Se instala json-server y sass para usar en el proyecto. Se incorpora la API para su uso con json-server, así como el primer bosquejo de la información para ver si la información se pinta adecuadamente con los botones de intercambiar entre estudiantes y staff.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import '../src/assets/styles/grid.scss'
 
 function App() {
+
+  const [informacion, setInformacion] = useState([])
+  const [filtro, setFiltro] = useState(0)
+
+  useEffect(() => {
+    let url = "http://localhost:3001/hp-students";
+    if (filtro !== 0) {
+      url = "http://localhost:3001/hp-staff"
+    }
+
+    fetch(url)
+      .then(resp => resp.json())
+      .then(
+        (data) => setInformacion(data),
+        (error) => console.log(error)
+      )
+  }, [filtro])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button type="button" onClick={() => setFiltro(0)}>Estudiantes</button>
+      <button type="button" onClick={() => setFiltro(1)}>Staff</button>
+
+      <div className="grid-flex">
+        {informacion.length !== 0 && (
+          informacion.map((dato) => (
+            <div className="linea-flex">
+              <div>
+                <img src={dato.image} />
+              </div>
+              <div>
+                <p>Nombre: {dato.name}</p>
+                <p>Cumpleaños: {dato.dateOfBirth}</p>
+                <p>Genero: {dato.gender}</p>
+                <p>Color de ojos: {dato.eyeColour}</p>
+                <p>Color de cabello: {dato.hairColour}</p>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+
+    </>
   );
 }
 
