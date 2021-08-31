b679d3df8f795732d18f8f044f24bf725fa1cdb9
Angel Castro <angelajco@gmail.com>
2021-08-31 03:48:33 -0500

    Se agrega la funcionalidad de agregar un nuevo personaje, así como mostrarlo en la lista y el diseño responsivo de los botones agregar y favoritos.

--- src/components/Personaje.js
+++ src/components/Personaje.js
@@ -10,6 +10,8 @@ import "../assets/styles/grid.scss"
 import { useSelector, useDispatch } from 'react-redux'
 import { añade, borra } from '../redux/reducer'
 
+import axios from 'axios'
+
 export default function Personaje(props) {
     //Selectores de Redux
     const contadorFavoritos = useSelector((state) => state.favoritoReducer.value)
@@ -26,7 +28,6 @@ export default function Personaje(props) {
 
     //Para mostrar si es favorito o no
     const clicFavorito = (dato) => {
-        console.log(dato, "datosaaaa")
         if (contadorFavoritos > 4) {
             alert("No puedes ingresar más de 5 personajes en favoritos")
         } else {
@@ -44,6 +45,7 @@ export default function Personaje(props) {
         }
     }
 
+    const [urlDato, setUrlDatos] = useState("")
     //Se ejecuta cada vez que el filtro que se pasa por props cambia, si se da clic en los botones de filtro
     useEffect(() => {
         //Por defecto muestra a los estudiantes
@@ -51,15 +53,18 @@ export default function Personaje(props) {
         if (props.filtro !== undefined && props.filtro !== 0) {
             url = "http://localhost:3001/hp-staff"
         }
-
+        setUrlDatos(url)
         //Se hace la petición
         fetch(url)
             .then(resp => resp.json())
             .then(
+                // (data) => setInformacion(data),
                 (data) => setInformacion(data),
                 (error) => console.log(error)
             )
     }, [props.filtro])
+
+
 
     const borraElemento = (dato) => {
         setMuestraFavorito(false)
@@ -80,6 +85,77 @@ export default function Personaje(props) {
         dispatch(borra(dato.payload))
     }
 
+    //Para guardar el valor del genero y de la posicion
+    const [genero, setGenero] = useState(0)
+    const [posicion, setPosicion] = useState(0)
+    //Para mostrar lista de favoritos
+    const [listaFavoritos, setListaFavoritos] = useState(false)
+
+    //Funcion que ejecuta el formulario
+    const agregaPersonaje = async (e) => {
+        e.preventDefault();
+        let nombre = e.target[0].value;
+        let cumpleaños = e.target[1].value
+        let ojos = e.target[2].value
+        let pelo = e.target[3].value
+        let foto = "";
+        if (e.target[8].files[0]) {
+            foto = URL.createObjectURL(e.target[8].files[0])
+        }
+
+        let posEstudiante = false;
+        let posStaff = false;
+        let urlGuardar;
+        let nombreGenero;
+        if (genero === 0) {
+            nombreGenero = "female"
+        } else if (genero === 1) {
+            nombreGenero = "male"
+        }
+        if (posicion === 0) {
+            posEstudiante = true
+            urlGuardar = "http://localhost:3001/hp-students";
+        } else if (posicion === 1) {
+            posStaff = true;
+            urlGuardar = "http://localhost:3001/hp-staff";
+        }
+
+        //Espera la llamada para renderizar los datos
+        await axios.post(urlGuardar, {
+            name: nombre,
+            dateOfBirth: cumpleaños,
+            gender: nombreGenero,
+            eyeColour: ojos,
+            hairColour: pelo,
+            hogwartsStudent: posEstudiante,
+            hogwartsStaff: posStaff,
+            image: foto,
+        }).then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+            console.log(error);
+        });
+        setMuestraModal(false)
+        //Se hace la petición de nuevo para renderizar el nuevo valor
+        fetch(urlDato)
+            .then(resp => resp.json())
+            .then(
+                (data) => setInformacion(data),
+                (error) => console.log(error)
+            )
+    }
+
+    // Para cambiar el genero cuando se le da clic
+    const cambiarGenero = (changeEvent) => {
+        setGenero(Number(changeEvent.target.value))
+    }
+
+    // Para cambiar la posicion cuando se le da clic
+    const cambiaPosicion = (changeEvent) => {
+        setPosicion(Number(changeEvent.target.value))
+    }
+
+
     return (
         <>
             {
@@ -88,54 +164,57 @@ export default function Personaje(props) {
                         <div className="modal-content">
                             <span className="close" onClick={() => setMuestraModal(false)}>&times;</span>
                             <p>Agrega un personaje</p>
-                            <form>
+                            <form className="form-modal" onSubmit={agregaPersonaje}>
                                 <div className="grupos-inputs">
                                     <div className="label-input">
-                                        <label for="nombre">NOMBRE</label>
-                                        <input type="text" name="nombre" />
+                                        <label htmlFor="nombre">NOMBRE</label>
+                                        <input className="inputs-grises" type="text" id="nombre" />
                                     </div>
                                     <div className="label-input">
-                                        <label form="cumpleaños">CUMPLEAÑOS</label>
-                                        <input name="cumpleaños" type="date" />
+                                        <label htmlFor="cumpleaños">CUMPLEAÑOS</label>
+                                        <input className="inputs-grises" id="cumpleaños" type="date" />
                                     </div>
                                 </div>
                                 <div className="grupos-inputs">
                                     <div className="label-input">
-                                        <label for="ojos">COLOR DE OJOS</label>
-                                        <input name="ojos" type="text" />
+                                        <label htmlFor="ojos">COLOR DE OJOS</label>
+                                        <input className="inputs-grises" id="ojos" type="text" />
                                     </div>
                                     <div className="label-input">
-                                        <label for="pelo">COLOR DE PELO</label>
-                                        <input name="pelo" type="text" />
+                                        <label htmlFor="pelo">COLOR DE PELO</label>
+                                        <input className="inputs-grises" id="pelo" type="text" />
                                     </div>
                                 </div>
                                 <div className="grupos-inputs">
-                                    <div>
-                                        <div>
-                                            <label for="genero">GÉNERO</label>
+                                    <div className="contenedores-labels-radios">
+                                        <div className="labels-radios">
+                                            <label>GÉNERO</label>
                                         </div>
                                         <div>
                                             <label>Mujer</label>
-                                            <input type="radio" name="genero" value={0} />
+                                            <input className="primer-radio" type="radio" name="genero" value={0} defaultChecked={genero === 0} onChange={cambiarGenero} />
                                             <label>Hombre</label>
-                                            <input type="radio" name="genero" value={1} />
+                                            <input type="radio" name="genero" value={1} defaultChecked={genero === 1} onChange={cambiarGenero} />
                                         </div>
                                     </div>
-                                    <div>
-                                        <div>
+                                    <div className="contenedores-labels-radios">
+                                        <div className="labels-radios">
                                             <label>POSICIÓN</label>
                                         </div>
                                         <div>
                                             <label>Estudiante</label>
-                                            <input type="radio" name="posicion" value={0} />
+                                            <input className="primer-radio" type="radio" name="posicion" value={0} defaultChecked={posicion === 0} onChange={cambiaPosicion} />
                                             <label>Staff</label>
-                                            <input type="radio" name="posicion" value={1} />
+                                            <input type="radio" name="posicion" value={1} defaultChecked={posicion === 1} onChange={cambiaPosicion} />
                                         </div>
                                     </div>
                                 </div>
                                 <div className="contenedor-fotografia">
-                                    <label>Fotografía</label>
+                                    <label>FOTOGRAFÍA</label>
                                     <input type="file" />
+                                </div>
+                                <div className="contenedor-boton-guardar">
+                                    <button className="boton-submit-guardar" type="submit">GUARDAR</button>
                                 </div>
                             </form>
                         </div>
@@ -144,29 +223,31 @@ export default function Personaje(props) {
             }
 
             <div className="botones-flotantes">
-                <button className="boton-fav">
-                    <div>FAVORITOS&nbsp;</div>
-                    <div>
-                        <img src={FavoritoBlanco} alt="Favoritos" />
-                    </div>
-                </button>
-                <button className="boton-agregar" onClick={() => setMuestraModal(true)}>
-                    <div>AGREGAR&nbsp;</div>
-                    <div>
-                        <img src={Agregar} alt="Agregar" />
-                    </div>
-                </button>
+                <div className="botones-absolutos">
+                    <button onClick={() => setListaFavoritos(!listaFavoritos)} className="boton-fav">
+                        <div>FAVORITOS&nbsp;</div>
+                        <div>
+                            <img src={FavoritoBlanco} alt="Favoritos" />
+                        </div>
+                    </button>
+                    <button className="boton-agregar" onClick={() => setMuestraModal(true)}>
+                        <div>AGREGAR&nbsp;</div>
+                        <div>
+                            <img src={Agregar} alt="Agregar" />
+                        </div>
+                    </button>
+                </div>
                 {
-                    personajesFavoritos.length !== 0 && (
+                    (personajesFavoritos.length !== 0 && listaFavoritos) && (
                         <>
                             <div className="personajes-favoritos">
                                 {
                                     personajesFavoritos.map((valor, index) => (
                                         <Fragment key={index}>
                                             <div className="contenedor-favoritos-miniatura" key={index}>
-                                                <img className="imagen-foto-miniatura" src={valor.payload.image} />
+                                                <img className="imagen-foto-miniatura" src={valor.payload.image} alt="foto-miniatura" />
                                                 <div>{valor.payload.name}</div>
-                                                <img className="imagen-eliminar" src={Borrar} onClick={() => borraElemento(valor)} />
+                                                <img alt="borrar" className="imagen-eliminar" src={Borrar} onClick={() => borraElemento(valor)} />
                                             </div>
                                             <hr className="separador" />
                                         </Fragment>
@@ -183,14 +264,14 @@ export default function Personaje(props) {
                     informacion.map((dato, index) => (
                         <div key={index} className="muestra-flex">
                             <div className={`contenedor-foto ${dato.house ? dato.house.toLowerCase() : "sin-casa"}`}>
-                                <img src={dato.image} className="imagen-foto" />
+                                <img src={dato.image} className="imagen-foto" alt="foto" />
                             </div>
                             <div className={`card-informacion ${dato.alive ? "" : "card-informacion-no-alive"}`}>
                                 <div className="favorito">
-                                    <p className="estado">{dato.alive == true ? "VIVO" : "FINADO"} / {dato.hogwartsStudent ? "ESTUDIANTE" : dato.hogwartsStaff ? "STAFF" : ""}</p>
+                                    <p className="estado">{dato.alive === true ? "VIVO" : "FINADO"} / {dato.hogwartsStudent ? "ESTUDIANTE" : dato.hogwartsStaff ? "STAFF" : ""}</p>
                                     {
                                         muestraFavorito && (
-                                            <img onClick={() => clicFavorito(dato)} src={dato.seleccionado ? Favorito : NoFavorito} />
+                                            <img alt="favorito" onClick={() => clicFavorito(dato)} src={dato.seleccionado ? Favorito : NoFavorito} />
                                         )
                                     }
                                 </div>
